7084eee15ec28c15091a08c966f4301ecb1d9283
kgrebenyuk <kgrebenyuk@luxoft.com>
2023-05-06 13:52:22 +0300

    HW36

--- src/main/java/com/example/demowithtests/domain/Employee.java
+++ src/main/java/com/example/demowithtests/domain/Employee.java
@@ -8,7 +8,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "users")
+//@Table(name = "users")
+@Table(name = "employees")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
---
+++ src/main/java/com/example/demowithtests/domain/Security/User.java
@@ -0,0 +1,18 @@
+package com.example.demowithtests.domain.Security;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    private String username;
+    private String password;
+    private Boolean enabled;
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<Authority> authority;
+}
